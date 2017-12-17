fe030db77ee4a68eee9c7c716796d182976a9b74
Asher Yartsev <asheryartsev@gmail.com>
2017-12-17 17:16:03 +0200

    better script

--- hw2/scripts/better_vs_random.py
+++ hw2/scripts/better_vs_random.py
@@ -5,17 +5,22 @@ better_wins_simple_when_second = 0
 better_wins_random_when_first = 0
 better_wins_random_when_second = 0
 
-extent = 10
+extent = 20
 weWantRanAlso = True
-for i in range(extent):
-    result = subprocess.check_output("python ../run_game.py 2 10 5 n better_player simple_player", shell=True)
-    if 'better' in result.decode("utf-8"):
-        better_wins_simple_when_first += 1
+weWantSimpleAlso = True
+if weWantSimpleAlso:
+    for i in range(1):
+        result = subprocess.check_output("python ../run_game.py 2 10 5 n better_player simple_player", shell=True)
+        if 'better' in result.decode("utf-8"):
+            better_wins_simple_when_first += 1
 
-for i in range(extent):
-    result = subprocess.check_output("python ../run_game.py 2 10 5 n simple_player better_player", shell=True)
-    if 'better' in result.decode("utf-8"):
-        better_wins_simple_when_second += 1
+    for i in range(1):
+        result = subprocess.check_output("python ../run_game.py 2 10 5 n simple_player better_player", shell=True)
+        if 'better' in result.decode("utf-8"):
+            better_wins_simple_when_second += 1
+    print("better won simple {0} times when was first".format(better_wins_simple_when_first))
+    print("better won simple {0} times when was second".format(better_wins_simple_when_second))
+
 if weWantRanAlso:
     for i in range(extent):
         result = subprocess.check_output("python ../run_game.py 2 10 5 n random_player better_player", shell=True)
@@ -28,8 +33,5 @@ if weWantRanAlso:
 
     print("better won random {0} times when was first".format(better_wins_random_when_first))
     print("better won random {0} times when was second".format(better_wins_random_when_second))
-
-print("better won simple {0} times when was first".format(better_wins_simple_when_first))
-print("better won simple {0} times when was second".format(better_wins_simple_when_second))
 
 
